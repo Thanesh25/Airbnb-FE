fce3148742b6c73911da3cc9f60950080cfd0ec3
Thaneshwar <Thaneshwar.js@gmail.com>
2024-09-08 09:18:34 +0530

    small change

--- src/Pages/PlacePage.jsx
+++ src/Pages/PlacePage.jsx
@@ -22,7 +22,7 @@ export default function PlacePage() {
 
 
   return (
-    <div className="mt-8 bg-gray-100 -mx-8 px-8 pt-8"> xx
+    <div className="mt-8 bg-gray-100 -mx-8 px-8 pt-8"> 
       <h1 className="text-2xl">{place.title}</h1>
       <AddressLink>{place.address }</AddressLink>
       <PlaceGallery place={ place} />
